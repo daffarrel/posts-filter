dd523ac21408bf9faeb8c7f1e378d9857ebf48ef
matej2 <11059438+matej2@users.noreply.github.com>
2019-10-19 17:43:11 +0200

    Code  refactoring Added filtering by comments and reactions

--- src/PostsFilter.js
+++ src/PostsFilter.js
@@ -7,10 +7,15 @@ class PostsFilter extends EventEmitter {
   }
 
   filterPosts(posts) {  
-    return posts.filter(curr => {      
-      return curr.message ? curr.message.match(process.env.POST_REGEX) : true && this.hasPicture(curr);
+    return posts.filter(curr => {     
+      console.log(this.hasKeyword(curr), this.hasPicture(curr), this.hasReactions(curr), this.hasComments(curr) );
+      return  this.hasKeyword(curr) &&
+              this.hasPicture(curr) &&
+              this.hasReactions(curr) && 
+              this.hasComments(curr);
     });
   }
+
 
   getPostsByTimeRange(posts, time) {
     // Time ranges are in minutes
@@ -18,6 +23,31 @@ class PostsFilter extends EventEmitter {
       const created = Math.round(new Date(curr.created_time).getTime() / 60000);            
       return created > Math.round((new Date() / 60000) - time);
     });
+  }
+
+  hasKeyword(post) {
+    if(!post)
+      return false;
+    else {
+      if(!post.message)
+        return true;// Skip if no message
+      else
+        return !!post.message.match(process.env.POST_REGEX);
+    }
+  }
+
+  hasReactions(post) {
+    if(!post)
+      return false;
+    else
+      return post.totalCount >= process.env.POST_MIN_REACTIONS;
+  }
+
+  hasComments(post) {
+    if(!post)
+      return false;
+    else
+      return post.comment_count >= process.env.POST_MIN_COMMENTS;
   }
   
   getResponse(message, error) {
@@ -28,7 +58,7 @@ class PostsFilter extends EventEmitter {
   }
 
   hasPicture(message) {
-    return message.full_picture && process.env.POST_PICTURE;
+    return !!(message.full_picture && process.env.POST_PICTURE);
   }
 
   sendPost(post) {
